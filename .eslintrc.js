408ab01830b367c67398c5a555a9aa7ec12752b8
Guido Bouman <guido@q42.nl>
2016-10-06 23:48:56 +0200

    Update to latest webpack template.

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,22 @@ module.exports = {
   plugins: [
     'html'
   ],
+  // check if imports actually resolve
+  'settings': {
+    'import/resolver': {
+      'webpack': {
+        'config': 'build/webpack.base.conf.js'
+      }
+    }
+  },
   // add your custom rules here
   'rules': {
     'import/no-unresolved': 0,
+    // don't require .vue extension when importing
+    'import/extensions': ['error', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
